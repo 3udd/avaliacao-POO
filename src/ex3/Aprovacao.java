cdc63f524636a53db9faf43939a725e76d51c10f
Eud <follmannedu@gmail.com>
2026-08-15 17:48:20 -0300

    array dos estudantes aprovados criado

--- src/ex3/Aprovacao.java
+++ src/ex3/Aprovacao.java
@@ -13,5 +13,16 @@ public class Aprovacao {
         if (quant == 0) {
             return null;
         }
+
+        ex2.Estudante[] aprovados = new ex2.Estudante[quant];
+        int slave=0;
+
+        for (ex2.Estudante estudante : turma) {
+            if (estudante.calculaMedia() >= 6.0) {
+                aprovados[slave] = estudante;
+                slave++;
+            }
+        }
+        return aprovados;
     }
 }
