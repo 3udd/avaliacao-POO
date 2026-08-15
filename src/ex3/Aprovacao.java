9441c9e428c1a2a0643292db73771d627f1e70d2
Eud <follmannedu@gmail.com>
2026-08-15 17:46:32 -0300

    método aprovados criado

--- src/ex3/Aprovacao.java
+++ src/ex3/Aprovacao.java
@@ -1,4 +1,17 @@
 package ex3;
 
 public class Aprovacao {
+    public static ex2.Estudante[] aprovados (ex2.Estudante[] turma) {
+        int quant=0;
+
+        for (ex2.Estudante estudante : turma) {
+            if (estudante.calculaMedia() >= 6.0) {
+                quant++;
+            }
+        }
+
+        if (quant == 0) {
+            return null;
+        }
+    }
 }
