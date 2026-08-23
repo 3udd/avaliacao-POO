868ef995c3127c5d5b48fe0f98bc1510047915fa
Eud <follmannedu@gmail.com>
2026-08-23 15:32:50 -0300

    método emprestar criado

--- src/ex5/Livro.java
+++ src/ex5/Livro.java
@@ -7,4 +7,14 @@ public class Livro extends Publicacao {
         super(titulo);
         this.disponivel = true;
     }
+
+    public void emprestar() {
+        if (this.disponivel) {
+            this.disponivel = false;
+            System.out.println("Livro '" + getTitulo() + "' emprestado com sucesso");
+        }
+        else {
+            System.out.println("O livro '" + getTitulo() + "' já foi emprestado");
+        }
+    }
 }
