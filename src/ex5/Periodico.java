242af6d83664374e5a37b667f8623f40d677da2a
Eud <follmannedu@gmail.com>
2026-08-23 15:38:16 -0300

    classe periodico criada

---
+++ src/ex5/Periodico.java
@@ -0,0 +1,10 @@
+package ex5;
+
+public class Periodico extends Publicacao {
+    private int volume;
+
+    public Periodico(String titulo, int volume) {
+        super(titulo);
+        this.volume = volume;
+    }
+}
